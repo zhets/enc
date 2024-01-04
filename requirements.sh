a53a819c91cb70c23707a05fc1892a7256cafb17
XDXL VPN STORES <142297155+zhets@users.noreply.github.com>
2024-01-04 20:28:00 +0700

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 apt install git python -y
 apt install nodejs npm -y
 npm install -g bash-obfuscate
 
-wget -q https://github.com/zhets/enc/raw/main/kf.py
+wget -q -O kf.py "https://github.com/zhets/enc/raw/main/kf.py"
 python3 kf.py
